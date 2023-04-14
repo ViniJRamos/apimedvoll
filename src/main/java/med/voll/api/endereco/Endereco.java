cc46de503c3c449a3930ff152eb0904e6f109b22
Vini J Ramos <pr.viniciusramos@gmail.com>
2023-04-13 21:46:31 -0300

    adicionando validação dos dados enviados no modulo de medico e endereco

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,6 +1,8 @@
 package med.voll.api.endereco;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,13 +14,25 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Endereco {
 
+    @NotBlank
     private String logradouro;
+
+    @NotBlank
     private String bairro;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{8}")
     private String cep;
-    private String numero;
-    private String complemento;
+
+    @NotBlank
     private String cidade;
+
+    @NotBlank
     private String uf;
+
+    private String numero;
+
+    private String complemento;
 
     public Endereco(DadosEndereco dados){
         this.logradouro = dados.logradouro();
